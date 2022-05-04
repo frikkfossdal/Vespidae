acc39ae478b3486ed242b49dd4d14c93e3f6fed9
Frikk H Fossdal <frikkfossdal@gmail.com>
2022-05-04 13:15:05 +0200

    Update OffsetComponent.cs
    added option to keep original curve in output list

--- Vespidae/Vespidae/1. CurveTools/OffsetComponent.cs
+++ Vespidae/Vespidae/1. CurveTools/OffsetComponent.cs
@@ -33,6 +33,7 @@ namespace Vespidae
             pManager.AddCurveParameter("curve", "crv", "Curve or curves to offset", GH_ParamAccess.list); 
             pManager.AddNumberParameter("Distance", "dist", "Distance to offset", GH_ParamAccess.item, 1);
             pManager.AddIntegerParameter("Amount", "amo", "Amount of times to offset curve. Default 1", GH_ParamAccess.item, 1);
+            pManager.AddBooleanParameter("Keep", "keep", "Keep original polygon in solution. Default True", GH_ParamAccess.item, true); 
             //pManager.AddPlaneParameter("OutputPlane", "pln", "Plane to output solution to", GH_ParamAccess.item, Plane.WorldXY);
         }
 
@@ -56,19 +57,24 @@ namespace Vespidae
             double distance = 0;
             int amount = 0;
             var pln = new Plane();
+            bool keep = true; 
 
             if (!DA.GetDataList("curve", offsetCurves))return;
             DA.GetData("Distance", ref distance);
             DA.GetData("Amount", ref amount);
+            DA.GetData("Keep", ref keep); 
             //DA.GetData("OutputPlane", ref pln);
 
             //hack: extract plane from first input curve
 
-            var output = new List<Polyline>(); 
+            var output = new List<Polyline>();
+            
 
             foreach (var crv in offsetCurves) {
+                
                 crv.TryGetPlane(out pln);
                 List<Polyline> offsetPolylines = ClipperTools.ConvertCurvesToPolylines(offsetCurves);
+                if (keep) output.AddRange(offsetPolylines); 
                 output.AddRange(ClipperTools.offset(offsetPolylines, amount, pln, distance, RhinoDoc.ActiveDoc.ModelAbsoluteTolerance));
             }
 
